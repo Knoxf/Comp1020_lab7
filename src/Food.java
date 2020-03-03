9c631f5fb80927649c0229cb55e64e30731b65b9
ZuhaoFang <knoxfang@outlook.com>
2020-03-03 09:30:25 -0600

    lab 7 update gived file

--- src/Food.java
+++ src/Food.java
@@ -1,66 +1,43 @@
 
 public class Food{
   
-    // basic info
-   private String name;
-   // related to nutrition
-   private int calories;
-   private Ingredient[] ingredients;
-   private int cost; 
-   private static int totalCount; // total amount of Food objects Created
+  // basic info
+  private String name;
+  // related to nutrition
+  private int weight;
+  private int calories;
+   
+  private int protein;
+  private int fat;
+  private int carbs;
+
+  private int sugar;
+  private double fibre;
+
+  private static int totalCount; // total amount of Food objects Created
    
    
    // Constructor
-   public Food(String newName, int newCalories, int newCost, Ingredient[] ingredientArray){
+   public Food(String newName,int weight, int newCalories,int protein, int carbs, int sugar, double fibre, int fat){
        // basic info
        name = newName;
        // related to nutrition
        calories = newCalories;
-       cost = newCost; 
-       
-       ingredients = ingredientArray; // Update from String to Ingredient
+
+       this.weight = weight;
+       this.carbs = carbs;
+       this.fat = fat;
+       this.protein = protein;
+       this.sugar = sugar;
+       this.fibre = fibre;
        
        totalCount++;
     }
     
     public String toString(){
-        return "Name: " + name + " Calories: " + calories + " Cost: " + cost;
-    }
-    
-    // method "getIngredients" that returns all the values in the array formatted as a string. 
-    public String getIngredients(){
-        String str ="Ingredients: ";
-
-        if(ingredients.length == 0){
-          str += "None";
-        }else if (ingredients.length == 1){ // one ingredient
-          str += ingredients[0];
-        }else{
-          // Handle the case of multiple
-          for( int i =0; i < ingredients.length-1; i++){
-            str = str + ingredients[i].toString() + ", ";
-          }
-          str = str + ingredients[ingredients.length-1].toString();
-        }
-        
-        return str;
-    }
-    
-    // method setIngredients which is an instance method that accepts an array of type String 
-    // and has a return type of void. This should replace the current ingredient list with the 
-    // ingredient list passed as a parameter. 
-    public void setIngredients(Ingredient[] newIngredients){
-        ingredients = newIngredients;
+        return "Name: " + name + " Calories: " + calories + " Protein: " + protein + " Carbs: " + carbs + " Fat: " + fat;
     }
 
-    public void addIngredient(Ingredient newIng){
-        Ingredient[] temp = new Ingredient[ingredients.length+1];
-        for(int i = 0 ; i < ingredients.length; i++){
-          temp[i] = ingredients[i];
-        }
-        temp[temp.length -1 ] = newIng;
-        ingredients = temp;
-    }
     
     public static int getCount(){
         return totalCount;
